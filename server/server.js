ab848e6680004046825879ecee9856550e375a8a
samirdewan1995 <samirdewan1995@gmail.com>
2022-03-11 14:37:23 +0000

    putting in the api stuff

--- server/server.js
+++ server/server.js
@@ -5,10 +5,6 @@ const Chat = require("./view/chatView")
 const cors = require("cors");
 // const RoomsJSON = "./data/db.json"
 // const Rooms = require("./data/db.json");
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require('socket.io');
-const io = new Server(server); //might need to make more of these to make different chatrooms
 
 app.use(express.json());
 app.use(cors());
@@ -23,22 +19,3 @@ app.get("/", (_req, res) => {
 app.listen(5050, () => {
     console.log(`App listening on port 5050`);
 })
-
-const users = {};
-
-io.on("connection", (socket) => {
-  socket.on("new-user", (name) => {
-    users[socket.id] = name;
-    socket.broadcast.emit("user-connected", name);
-  });
-  socket.on("send-chat-message", (message) => {
-    socket.broadcast.emit("chat-message", {
-      message: message,
-      name: users[socket.id],
-    });
-  });
-  socket.on("disconnect", () => {
-    socket.broadcast.emit("user-disconnected", users[socket.id]);
-    delete users[socket.id];
-  });
-});
